9fcb58d7758ed6d90de24858d458e985a5c5c03e
serhanyilmaz <srhnylmz_3458@hotmail.com>
2020-01-13 08:57:41 +0300

    vol1

---
+++ DriverFileInstallApp/DriverFileInstallApp/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DriverFileInstallApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text=="serhan" && textBox2.Text=="123")
+            {
+                MessageBox.Show("Giriş başarılı","BİLGİ PENCERESİ",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Giriş başarısız", "BİLGİ PENCERESİ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+    }
+}
---
+++ DriverFileInstallApp/DriverFileInstallApp/ProjectInstaller.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DriverFileInstallApp
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            //InitializeComponent();
+        }
+        public override void Install(IDictionary savedState)
+        {
+            base.Install(savedState);
+            //Add custom code here
+            try
+            {
+
+                string action = "";
+                string assemblyPath = "";
+                StringDictionary myStringDictionary = Context.Parameters;
+                if (Context.Parameters.Count > 0)
+                {
+                    foreach (string myString in Context.Parameters.Keys)
+                    {
+                        if (myString.ToLower().Equals("action"))
+                        {
+                            action = Context.Parameters[myString];
+                        }
+                        if (myString.ToLower().Equals("assemblypath"))
+                        {
+                            assemblyPath = Context.Parameters[myString];
+                        }
+                    }
+                    //remove last file name to get the assembly root path
+                    assemblyPath = assemblyPath.Substring(0, assemblyPath.LastIndexOf("\\"));
+                }
+                string path_to_inf = assemblyPath + "\\driversFolder\\sign_inf\\smb.inf";
+                driverInstall(path_to_inf);
+                //}
+            }
+            catch { }
+        }
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+            //Add custom code here
+        }
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+            //Add custom code here
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+            //Add custom code here
+        }
+        /// <summary>
+        /// execute PnPutil.exe to install the driver INF file
+        /// </summary>
+        /// <param name="driverPath"></param>
+        private static void driverInstall(string driverPath)
+        {
+            try
+            {
+                var process = new Process();
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.FileName = "cmd.exe";
+                string a = @"" + driverPath + "";
+                process.StartInfo.Arguments = "/c C:\\Windows\\System32\\PnPutil.exe -a \"" + driverPath + "\""; // where driverPath is path of .inf file
+                process.Start();
+                process.WaitForExit();
+                process.Dispose();
+                Console.WriteLine(@"Driver has been installed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+}
